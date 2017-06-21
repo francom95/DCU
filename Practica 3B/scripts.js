20bfae35b7f1c5b6c0371031ca0d1132da533b62
Franco Montanari <fmontanari@linti.unlp.edu.ar>
2017-06-21 01:42:22 -0300

    Se modificaron todos los archivos y se agregaron nuevos de modo que el sitio no tenga enlaces erroneos. Tambien se modificaron otros aspectos.

---
+++ Practica 3B/scripts.js
@@ -0,0 +1,15 @@
+function formDos(resultado){
+	if(resultado=='continuar'){
+		setTimeout("location.href='https://dcu-unlp-2017.000webhostapp.com/RealizarDenuncia3.html'", 1);
+    }else{
+        setTimeout("location.href='https://dcu-unlp-2017.000webhostapp.com/realizar-denuncia.html'", 1);
+    }
+}
+
+function formTres(resultado){
+	if(resultado=='continuar'){
+		setTimeout("location.href='https://dcu-unlp-2017.000webhostapp.com/finalizar-denuncia.html'", 1);
+    }else{
+        setTimeout("location.href='https://dcu-unlp-2017.000webhostapp.com/RealizarDenuncia2.html'", 1);
+    }
+}
